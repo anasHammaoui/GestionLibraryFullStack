b723757a4ce611187afb31090716d110e7cbeadd
ikrambenallali <ikrambenallali2003@gmail.com>
2024-12-26 09:31:38 +0100

    GB-11 complete sign up

--- model/signUp.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include "database.php";
-    $database = new Database("localhost", "library","root","");
-     $database->conn();
-     $connection = $database -> setConn() ;
-    var_dump($connection);
-?>
