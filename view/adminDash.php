00b76574384c7cd4b8563fb84cc0b52f3e11a102
ikrambenallali <ikrambenallali2003@gmail.com>
2024-12-27 15:42:48 +0100

    crud users

--- view/adminDash.php
+++ view/adminDash.php
@@ -48,7 +48,7 @@ include "../controller/crudUsers.php";
               aria-hidden="true"></span>
             <a
               class="inline-flex items-center w-full text-sm font-semibold text-gray-800 transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200 dark:text-gray-100"
-              href="index.html">
+              href="adminDash.php">
               <svg
                 class="w-5 h-5"
                 aria-hidden="true"
@@ -90,6 +90,24 @@ include "../controller/crudUsers.php";
             </a>
           </li>
 
+          <li class="relative px-6 py-3">
+            <a
+              class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+              href="categoriesPage.php">
+              <svg
+                class="w-5 h-5"
+                aria-hidden="true"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
+              </svg>
+              <span class="ml-4">Categories</span>
+            </a>
+          </li>
         </ul>
         </template>
         </li>
